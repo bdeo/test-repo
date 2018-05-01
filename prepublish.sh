13bbf2c0421076fca6ef375d609c96194ec3a37b
Brandon Deo <bdeo@udel.edu>
2018-05-01 17:00:52 -0400

    dont edit message

--- prepublish.sh
+++ prepublish.sh
@@ -3,10 +3,10 @@ branchname="release-$version"
 git push -u origin $branchname
 git checkout production
 git reset --hard
-git merge --no-ff $branchname
+git merge --no-ff --no-edit $branchname
 git push
 git push --tags
 git checkout master
-git merge --no-ff $branchname
+git merge --no-ff --no-edit $branchname
 git push
 npm run build
