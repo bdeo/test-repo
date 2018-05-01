ed73f168c0a9fe9a0c7cb4a3eba6bf1ddc7ed324
Brandon Deo <bdeo@udel.edu>
2018-05-01 16:40:26 -0400

    simplify with branch var

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
-git push -u "release-$(node -p -e "require('./package.json').version")"
+$branchname = "release-$(node -p -e "require('./package.json').version")"
+git push -u origin $branchname
 git checkout production
 git reset --hard
-git merge --no-ff "release-$(node -p -e "require('./package.json').version")"
+git merge --no-ff $branchname
 git push
 git push --tags
 git checkout master
-git merge --no-ff "release-$(node -p -e "require('./package.json').version")"
+git merge --no-ff $branchname
 git push
 npm run build
