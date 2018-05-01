aad143c4a8f24485072e5e1bddc97d48bb68ac02
Brandon Deo <bdeo@udel.edu>
2018-05-01 16:35:12 -0400

    initial

---
+++ publish.sh
@@ -0,0 +1,10 @@
+git push -u "release-$(node -p -e 'require('./package.json').version')"
+git checkout production
+git git reset --hard
+git merge --no-ff "release-$(node -p -e 'require('./package.json').version')"
+git push
+git push --tags
+git checkout master
+git merge --no-ff "release-$(node -p -e 'require('./package.json').version')"
+git push
+npm run build
